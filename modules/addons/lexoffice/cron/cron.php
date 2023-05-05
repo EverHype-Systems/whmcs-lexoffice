c3887de56080ab7b002d1fcddc091839a5cc8cc2
Adrijan Bajrami <abajrami@everhype-systems.eu>
2023-05-05 22:37:54 +0200

    create a file for running the cron separatly

--- modules/addons/lexoffice/cron/cron.php
+++ modules/addons/lexoffice/cron/cron.php
@@ -1,32 +1,14 @@
 <?php
 
-require_once __DIR__ . '/../LexofficeHelper.php';
+require_once __DIR__ . '/../../../init.php';
 
-class CronHelper{
-    public static function doDailyCron() {
-        # get all paid invoices in lexoffice
-        $start_from = LexofficeModuleAccessHelper::getStartInvoice();
-        if ($start_from == null || strlen($start_from) == 0 || !strtotime($start_from)){
-            $start_from = '01.01.2019';
-        }
-
-        $checkDATE = strtotime($start_from);
-        $invoicesUpload = [];
-        $paidInvoices = InvoicesHelper::getPaidInvoices();
-        # drop all paid invoices which are already integrated
-        foreach ($paidInvoices as $invoice){
-            $invoiceModel = new LexofficeInvoice($invoice->id);
-            if (!$invoiceModel->isIntegrated() && $invoiceModel->isPaid()){
-                try {
-                    if (strtotime($invoice->date) > $checkDATE){
-                        echo "Rechnung wird nun syncronsiert -> #" . $invoice->id . PHP_EOL;
-                        $invoiceModel->integrateInvoice();
-                    }
-                } catch (Exception $e) {
-                    echo PHP_EOL . PHP_EOL . 'ERROR: ' . $e->getMessage() . PHP_EOL . PHP_EOL;
-                }
-            }
-        }
-    }
+# allow only from cli
+if (php_sapi_name() !== 'cli') {
+    die('CLI only');
 }
 
+use helpers\CronHelper;
+
+$cron = new CronHelper();
+
+$cron->run();
