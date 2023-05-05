bde0ae055d1310acd2bd7d2f4c51bcac584500cc
Adrijan Bajrami <abajrami@everhype-systems.eu>
2023-05-05 22:37:41 +0200

    updating hooks for refactor

--- modules/addons/lexoffice/hooks.php
+++ modules/addons/lexoffice/hooks.php
@@ -1,22 +1,35 @@
 <?php
 
-require_once dirname(__FILE__) . '/LexofficeHelper.php';
+use helpers\CronHelper;
+use helpers\LexofficeClient;
+use helpers\LexofficeModuleHelper;
 
-add_hook('DailyCronJob', 1, function($vars) {
+add_hook('DailyCronJob', 1, function ($vars) {
     try {
         echo "lexoffice Export wird gestartet..." . PHP_EOL;
-        CronHelper::doDailyCron();
+        $cron = new CronHelper();
+        $cron->run();
     } catch (Exception $e) {
         echo "Fehler bei der Ausführung: " . $e->getMessage();
     }
 });
 
-add_hook('ClientAdd', 1, function($vars) {
-    $client = new LexofficeClient($vars);
+add_hook('ClientAdd', 1, function ($vars) {
+    # create an instance of LexofficeModuleHelper
+    $lexofficeModuleHelper = new LexofficeModuleHelper();
+
+    # create an instance of Lexoffice Client for the user
+    $client = new LexofficeClient($vars, $lexofficeModuleHelper->getLexofficeClient());
+
     $client->integrate();
 });
 
-add_hook('ClientEdit', 1, function($vars) {
-    $client = new LexofficeClient($vars);
+add_hook('ClientEdit', 1, function ($vars) {
+    # create an instance of LexofficeModuleHelper
+    $lexofficeModuleHelper = new LexofficeModuleHelper();
+
+    # create an instance of Lexoffice Client for the user
+    $client = new LexofficeClient($vars, $lexofficeModuleHelper->getLexofficeClient());
+
     $client->integrate();
 });
